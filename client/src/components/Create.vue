b6cb8ef8d96fc6297a5e5d016c832ed3b8a5b6bc
Micklerick617 <mlennon@smu.edu>
2022-02-12 16:33:46 -0600

    Quasar Formatting for Create Page

--- client/src/components/Create.vue
+++ client/src/components/Create.vue
@@ -19,18 +19,19 @@
 
     <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
       <q-label for="create-from">From</q-label>
-      <q-input  type="text" id="create-from" name="name" required
-              minlength="4" maxlength="8" size="10"></q-input>
+      <q-input outlined class="q-my-xs q-mx-sm" type="text" id="create-from" name="name" required
+              minlength="4" maxlength="50" size="10"></q-input>
 
       <q-label for="create-to">To</q-label>
-      <q-input  type="text" id="create-to" name="name" required
-              minlength="4" maxlength="8" size="10"></q-input>
+      <q-input outlined class="q-my-xs q-mx-sm" type="text" id="create-to" name="name" required
+              minlength="4" maxlength="50" size="10"></q-input>
 
-      <q-label for="create-message">Message</q-label>
-      <q-input  type="text" id="create-message" name="name" required
-              minlength="4" maxlength="8" size="10"></q-input>
+      <q-label for="create-message">Message (250 Characters)</q-label>
+      <q-input outlined class="q-my-xs q-mx-sm" 
+              type="textarea" id="create-message" name="name" required
+              minlength="4" maxlength="250" size="10"></q-input>
 
-      <q-btn type="button">Generate</q-btn>
+      <q-btn type="button" class="q-my-lg">Generate</q-btn>
     </q-drawer>
 
     <q-page-container>
@@ -42,7 +43,18 @@
 </template>
 
 <script>
+import { ref } from 'vue'
+
 export default {
   name: 'Create',
+  setup () {
+    const leftDrawerOpen = ref(false)
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
 }
 </script>
