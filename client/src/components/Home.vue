2e28dba50af1f36a3104be5bf264d3777c2a0504
jadonstronge <78332067+jadonstronge@users.noreply.github.com>
2022-02-12 17:34:45 -0600

    centered background and reroute button

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,10 +1,14 @@
 <template>
-  <div>
-    WELCOME TO THE LOVE CARD 
-
+  <div class = "home-page">
+    
+      <router-link class= "absolute-center create-card" to="/create">
+         <button type = "button"> 
+            Create a Card
+        </button>    
+      </router-link>
 
     <img class="background" src="../assets/neon_heart.gif" alt="Neon Heart Gif">
-        <router-link to="/create"> <p>Create a Card </p> </router-link>
+        
     <router-view></router-view>
   </div>
 </template>
@@ -17,8 +21,29 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+  .home-page{
+    position: relative;
+    height: 100%;
+  }
+
+  .create-card {
+    margin: 0;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    -ms-transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%);
+  }
+
   .background{
+    position: absolute;
+    z-index: -1;
     width: 100%;
     height: 100%;
+    top: 50%;
+    left: 50%;
+    -ms-transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%);
   }
 </style>
