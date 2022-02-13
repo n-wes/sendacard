940e237c83cc5acf421432df4af177a5aa3654ae
TrevorDohm <tdohm@smu.edu>
2022-02-12 19:48:35 -0600

    Updating Read with top.

--- client/src/components/Read.vue
+++ client/src/components/Read.vue
@@ -1,9 +1,21 @@
 <template>
   <div>
-    WELCOME TO Read PAGE
+    <q-layout view="hHh lpR fFf">
+      <q-header elevated class="bg-primary text-white">
+        <q-toolbar>
+          <q-toolbar-title>
+            <q-avatar>
+              <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+            </q-avatar>
+            Title
+          </q-toolbar-title>
+        </q-toolbar>
+      </q-header>
+    </q-layout>
   </div>
 </template>
 
+<!-- Add previewing from "Preview.vue" here -->
 <script>
 export default {
   name: 'Read',
