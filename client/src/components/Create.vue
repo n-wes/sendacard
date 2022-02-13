260afb5210c092270a6707df9ea1b421ee85c026
Micklerick617 <mlennon@smu.edu>
2022-02-12 18:53:34 -0600

    Preparing to merge

--- client/src/components/Create.vue
+++ client/src/components/Create.vue
@@ -61,16 +61,12 @@ export default {
   // Methods are functions that mutate state and trigger updates.
   // They can be bound as event listeners in templates.
   methods: {
-    increment() {
-      this.count++
-    }
   },
 
   // Lifecycle hooks are called at different stages
   // of a component's lifecycle.
   // This function will be called when the component is mounted.
   mounted() {
-    console.log(`The initial count is ${this.count}.`)
   }
 }
 </script>
