c06052576f5789c57e7e1d7be653cca75b8cafb7
Micklerick617 <mlennon@smu.edu>
2022-02-13 00:04:57 -0600

    Using UseRoute()

--- client/src/components/Read.vue
+++ client/src/components/Read.vue
@@ -1,11 +1,15 @@
 <template>
   <div>
-    WELCOME TO Read PAGE
+    <Preview :cardType="v-1" :frontMsg="this.front" :mainMsg="this.main" :backMsg="this.back"></Preview> 
   </div>
 </template>
 
 <script>
+import Preview from './Preview.vue';
+import { useRoute } from 'vue-router'
+
 export default {
+  components: { Preview },
   name: 'Read',
   data(){
     return{
@@ -13,19 +17,22 @@ export default {
       urlArgs: {}
     }
   },
-  mounted() {
+  setup() {
 
-      console.log(this.$route);
-      this.urlArgs = {
-        type: this.$route.query.type,
-        front: this.$route.query.front,
-        main: this.$route.query.main,
-        back: this.$route.query.back
-      }
+      const route = useRoute()
 
-      this.urlArgs.front = this.urlArgs.front.replace(/`/g, " ");
-      this.urlArgs.main = this.urlArgs.main.replace(/`/g, " ");
-      this.urlArgs.back = this.urlArgs.back.replace(/`/g, " ");
+      console.log(route);
+
+      // this.urlArgs = {
+      //   type: this.$route.query.type,
+      //   front: this.$route.query.front,
+      //   main: this.$route.query.main,
+      //   back: this.$route.query.back
+      // }
+
+      // this.urlArgs.front = this.urlArgs.front.replace(/`/g, " ");
+      // this.urlArgs.main = this.urlArgs.main.replace(/`/g, " ");
+      // this.urlArgs.back = this.urlArgs.back.replace(/`/g, " ");
   },
   props: ['messageString']
 }
