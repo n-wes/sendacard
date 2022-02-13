603b8d8b5e2cdc2d5b470ed7ac1b95509be96612
Micklerick617 <mlennon@smu.edu>
2022-02-12 23:26:34 -0600

    Preparing to merge

--- client/src/components/Read.vue
+++ client/src/components/Read.vue
@@ -7,6 +7,27 @@
 <script>
 export default {
   name: 'Read',
+  data(){
+    return{
+      title: 'Read',
+      urlArgs: {}
+    }
+  },
+  mounted() {
+
+      console.log(this.$route);
+      this.urlArgs = {
+        type: this.$route.query.type,
+        front: this.$route.query.front,
+        main: this.$route.query.main,
+        back: this.$route.query.back
+      }
+
+      this.urlArgs.front = this.urlArgs.front.replace(/`/g, " ");
+      this.urlArgs.main = this.urlArgs.main.replace(/`/g, " ");
+      this.urlArgs.back = this.urlArgs.back.replace(/`/g, " ");
+  },
+  props: ['messageString']
 }
 </script>
 
