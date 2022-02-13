952d103c1e5a53bd7b165bcd7871149782141859
Nathan <nathanswesley@gmail.com>
2022-02-12 22:14:54 -0600

    redesign home page

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,17 +1,9 @@
 <template>
-  <div class = "home-page">
-
-    <h3 class = "template">
-    Welcome to the LOVE CARD 
-    </h3>
-      <router-link class= "absolute-center create-card" to="/create">
-         Create Card <br>
-         click me 
-      </router-link>
-
-    <img class="background" src="../assets/red_heart.png" alt="Neon Heart Gif">
-        
-    <router-view></router-view>
+  <div class="container">
+    <span class="title">Welcome to Send-a-Card!</span>
+    <router-link to=/create>
+      <q-btn class="btn" color="red" text-color="white" label="Create a card!"/>
+    </router-link>
   </div>
 </template>
 
@@ -23,42 +15,21 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
-  .template {
-    display: block;
-    font-size: 2em;
-    color:black;
-    margin-block-start: 0em;
-    margin-block-end: 0em;
-    margin-inline-start: 0px;
-    margin-inline-end: 0px;
-    font-weight: bold;
-  }
-
-  .home-page{
-    position: relative;
+  .container {
     height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-content: center;
+    justify-content: center;
   }
-
-  .create-card {
-    margin: 0;
+  .title {
     font-size: 4rem;
-    font-family: 'Times New Roman', Times, serif;
-    text-decoration: none;
-    color: white;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    -ms-transform: translate(-50%, -50%);
-    transform: translate(-50%, -50%);
   }
-
-  .background{
-    position: absolute;
-    z-index: -1;
-    top: 50%;
-    left: 50%;
-    -ms-transform: translate(-50%, -50%);
-    transform: translate(-50%, -50%);
+  .btn {
+    margin-top: 10rem;
+    font-size: 5rem;
+    padding: 3rem;
+    width: 60rem;
+    height: 20rem;
   }
 </style>
